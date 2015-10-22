7ed5438a33a6b72e4b13e72485dfb217ecf21877
John Costa <johncosta@Johns-MacBook-Pro.local>
2015-10-22 10:19:36 -0700

    ready for heroku deploy attempt #1

--- models/index.js
+++ models/index.js
@@ -1,5 +1,8 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/my_heroku_app");
+
+mongoose.connect( process.env.MONGOLAB_URI ||
+                  process.env.MONGOHQ_URL || 
+                  "mongodb://localhost/project_1" );
 
 // After creating a new model, require and export it:
 // module.exports.Tweet = require("./tweet.js");
